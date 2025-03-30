b2fa472e8e349a689189e91a1eb02a7516bed3a4
Michele Vitucci <michele.vitucci99@gmail.com>
2025-03-30 16:35:54 +0200

    Add files via upload

---
+++ ECMAScript-modules/Hello.js
@@ -0,0 +1,7 @@
+// Definiamo una funzione
+function Hello() {
+    console.log("Hello world1!");
+}
+
+// Esportiamo la funzione con export default
+export default Hello;
---
+++ ECMAScript-modules/Import1.js
@@ -0,0 +1,5 @@
+// Importiamo la funzione con import
+import Hello from "./Hello"; // NOTA: serve l'estensione .js nei moduli ES6
+
+// Chiamiamo la funzione
+Hello();
